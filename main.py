f88a6786d7ee30964bda2afe5d36413e17d00c6a
FrancescoCesari97 <freedus97@gmail.com>
2024-06-26 17:27:11 +0200

    deleting file in python

--- main.py
+++ main.py
@@ -4,64 +4,85 @@ import os
 
 # * file detection
 
-path = "C:\\Users\\freed\\Desktop\\test1.txt"
+# path = "C:\\Users\\freed\\Desktop\\test1.txt"
 
-if os.path.exists(path):
-    print("that location exists!")
-    if os.path.isfile(path):
-        print("this is a file")
-    elif os.path.isdir(path):
-        print("that is a directory")
-else:
-    print("that location dosn't exist")
+# if os.path.exists(path):
+#     print("that location exists!")
+#     if os.path.isfile(path):
+#         print("this is a file")
+#     elif os.path.isdir(path):
+#         print("that is a directory")
+# else:
+#     print("that location dosn't exist")
 
 
 
 # * file reading in python
 
-try:
-    with open('text.txt') as file:
-        print(file.read())
-except FileNotFoundError:
-    print("That file was not found :(")
+# try:
+#     with open('text.txt') as file:
+#         print(file.read())
+# except FileNotFoundError:
+#     print("That file was not found :(")
 # print(file.closed)
 
 
 # * file writing in python
 
 
-text = "prova scrittura\n prova scrittura test"
+# text = "prova scrittura\n prova scrittura test"
 
-# 'r' for reading
-# 'w' for overwriting
-# 'a' for append / writing after what's already written 
+#* 'r' for reading
+#* 'w' for overwriting
+#* 'a' for append / writing after what's already written 
 
-with open('text.txt', 'a')as file:
-    file.write(text)
+# with open('text.txt', 'a')as file:
+#     file.write(text)
 
 
 # * file copyng in python
 
-    # copyfile() = copies contents of a file
-    # copy() = copyfile() + permission mode + destination can be a directory 
-    # copy2() = copy() + copies metadata (files's creation and modification times)
+    # *copyfile() = copies contents of a file
+    # *copy() = copyfile() + permission mode + destination can be a directory 
+    # *copy2() = copy() + copies metadata (files's creation and modification times)
+
+# import shutil
+
+# shutil.copyfile('test.txt', 'text.txt') #src and dst
+    
+
+# * moving file with python
+
+# source = "test.txt"
+# destination = "D:\\corso web developer\\test.txt"
+
+
+# try: 
+#     if os.path.exists(destination):
+#         print("There is already a file there")
+#     else:
+#         os.replace(source, destination)
+#         print(source + "was moved")
+# except FileNotFoundError:
+#     print(source + "was not found")
+
+
+# * deleting file in python
 
 import shutil
 
-shutil.copyfile('test.txt', 'text.txt') #src and dst
-    
+path = "folder"
 
-# * moving file in python
+try:
+    # os.remove(path) -> for deleting file
+    # os.rmdir(path) -> for deleting empty folders
+    shutil.rmtree(path) # -> for delintig folders with files in it 
 
-source = "test.txt"
-destination = "D:\\corso web developer\\test.txt"
-
-
-try: 
-    if os.path.exists(destination):
-        print("There is already a file there")
-    else:
-        os.replace(source, destination)
-        print(source + "was moved")
 except FileNotFoundError:
-    print(source + "was not found")
+    print("That file was not found")
+except PermissionError:
+    print("You do not have the permission to delete that")
+except OSError:
+    print("you cannot delete folder with file in it with that function")
+else:
+    print(path + " was deleted")
